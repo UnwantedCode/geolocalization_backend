12e8e07edce23aac2c4d5d1a0f30ad29de957af7
AdamTarkowski <adamtarkowski2001@gmail.com>
2026-01-15 08:36:02 +0100

    Dodanie do grupy za pomocą kodu

--- src/Controller/Api/JoinGroupController.php
+++ src/Controller/Api/JoinGroupController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/user/join-group', name: 'api_user_join_group', methods: ['POST'])]
 class JoinGroupController extends AbstractController
 {
-    public function __invoke(
+    #[Route('/api/user/join-group', name: 'api_user_join_group', methods: ['POST'])]
+    public function joinByCode(
         Request $request,
         GroupRepository $groupRepo,
         Security $security,
@@ -35,10 +35,40 @@ class JoinGroupController extends AbstractController
             return $this->json(['error' => 'User is already a member of this group'], Response::HTTP_CONFLICT);
         }
 
+        $group->addUser($user);
+        $entityManager->persist($group);
+        $entityManager->flush();
+        return $this->json(['message' => 'User added to group successfully'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/user/join-group-code', name: 'api_user_join_group_code', methods: ['POST'])]
+    public function joinByGroupCode(
+        Request $request,
+        GroupRepository $groupRepo,
+        Security $security,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $user = $security->getUser();
+        $data = json_decode($request->getContent(), true);
+        $groupCode = $data['code'] ?? null;
+
+        if (!$groupCode) {
+            return $this->json(['error' => 'Group code is required'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $group = $groupRepo->findOneBy(['code' => $groupCode]);
+        if (!$group) {
+            return $this->json(['error' => 'Group not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($group->getUsers()->contains($user)) {
+            return $this->json(['error' => 'User is already a member of this group'], Response::HTTP_CONFLICT);
+        }
 
         $group->addUser($user);
         $entityManager->persist($group);
         $entityManager->flush();
+
         return $this->json(['message' => 'User added to group successfully'], Response::HTTP_CREATED);
     }
 }
